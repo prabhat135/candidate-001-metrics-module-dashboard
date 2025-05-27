a628facdf62ee29d9d5439ffa8ca58829f958d84
prabhat135 <jainprabhat807@gmail.com>
2025-05-27 17:10:35 +0530

    Initial commit

---
+++ api/raffleSales.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+  res.json({
+    ticketsSold: 350,
+    revenueCents: 35000,
+    avgBoostPerUser: 5
+  });
+};
---
+++ api/upgradeConversions.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+  res.json({
+    boostSales: 120,
+    premiumSales: 45,
+    conversionRate: 7.5
+  });
+};
